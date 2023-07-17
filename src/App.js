5898030f125a4ba7ca92ecaa8453f1c8717994a2
Edgard Antonorsi <edg.ant@gmail.com>
2023-07-17 13:31:10 -0400

    Adjust styles. Remove example elements

--- src/App.js
+++ src/App.js
@@ -3,46 +3,20 @@ import { useState } from "react";
 
 const App = () => {
   const iframeStyle = {
-    border: "2px solid red",
+    height: '100.1vh',
+    width: '100%',
   };
+  const [setRetoolData] = useState("");
+  const [data] = useState("");
 
-  const sample = {
-    example1: "",
-    example2: false,
-    input: "",
-  };
-
-  const [retoolData, setRetoolData] = useState("");
-  const [data, setData] = useState(sample);
   return (
     <div>
-      <h1> React-Retool</h1>
-      <button
-        onClick={() => {
-          setData({ ...data, example2: !data.example2 });
-        }}
-      >
-        Click me!
-      </button>
-      <br />
-      <br />
-      <label> Share something: </label>
-      <input
-        type="text"
-        value={data.input}
-        onChange={(e) => setData({ ...data, input: e.target.value })}
-      />
-      <br />
-      <br />
       <Retool
         url="https://support.retool.com/embedded/public/cb9e15f0-5d7c-43a7-a746-cdec870dde9a"
         data={data}
-        height="700px"
-        width="1000px"
         onData={setRetoolData}
         styling={iframeStyle}
       ></Retool>
-      <h1> {JSON.stringify(retoolData)} </h1>
     </div>
   );
 };
